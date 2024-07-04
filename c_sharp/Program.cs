3225498c7fcdec23b2e30c30a1f291e66765cb29
Mohit Sinha <mohitsinha1994@gmail.com>
2024-07-04 18:18:58 +0530

    first commit

---
+++ c_sharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class AddNumbers
+{
+    static int AddTwoNumbers(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    static void Main()
+    {
+        int number1 = 5;
+        int number2 = 10;
+        int result = AddTwoNumbers(number1, number2);
+        
+        Console.WriteLine("The sum of {0} and {1} is {2}", number1, number2, result);
+    }
+}
